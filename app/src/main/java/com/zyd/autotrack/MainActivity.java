47e601fcd0ab59e4553c38dd073d619440423d3c
zhengyanda <zhengyanda@meituan.com>
2018-12-19 10:16:38 +0800

    织入Toast
    Signed-off-by: zhengyanda <zhengyanda@meituan.com>

--- app/src/main/java/com/zyd/autotrack/MainActivity.java
+++ app/src/main/java/com/zyd/autotrack/MainActivity.java
@@ -1,13 +1,29 @@
 package com.zyd.autotrack;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.zyd.track.Cost;
 
+
+public class MainActivity extends AppCompatActivity{
+
+    private Button button;
+
+    @Cost
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("onclick");
+            }
+        });
     }
 }
--- track/src/main/groovy/com/zyd/track/AutoTrackPlugin.groovy
+++ track/src/main/groovy/com/zyd/track/AutoTrackPlugin.groovy
@@ -1,12 +1,85 @@
 package com.zyd.track
 
+import com.android.build.api.transform.*
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.internal.pipeline.TransformManager
+import com.android.tools.r8.org.objectweb.asm.ClassReader
+import com.android.tools.r8.org.objectweb.asm.ClassVisitor
+import com.android.tools.r8.org.objectweb.asm.ClassWriter
+import com.android.utils.FileUtils
+import org.apache.commons.codec.digest.DigestUtils
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class AutoTrackPlugin implements Plugin<Project> {
+class AutoTrackPlugin extends Transform implements Plugin<Project> {
+
+    public final String TAG = "AutoTrackPlugin"
+
     void apply(Project project) {
-        System.out.println("------------------开始----------------------")
-        System.out.println("这是我们的自定义插件!");
-        System.out.println("------------------结束-----------------------")
+
+        def android = project.extensions.getByType(AppExtension)
+        android.registerTransform(this)
+
+    }
+
+    @Override
+    String getName() {
+        return TAG
+    }
+
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS
+    }
+
+    @Override
+    Set<? super QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT
+    }
+
+    @Override
+    boolean isIncremental() {
+        return false
+    }
+
+    @Override
+    void transform(Context context, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs,
+                   TransformOutputProvider outputProvider, boolean isIncremental) throws IOException, TransformException, InterruptedException {
+        inputs.each { TransformInput input ->
+            input.directoryInputs.each { DirectoryInput directoryInput ->
+                if (directoryInput.file.isDirectory()) {
+                    directoryInput.file.eachFileRecurse { File file ->
+                        def name = file.name
+                        if (name.endsWith(".class") && !name.startsWith("R\$") &&
+                                "R.class" != name && "BuildConfig.class" != name) {
+                            ClassReader cr = new ClassReader(file.bytes)
+                            ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
+                            ClassVisitor cv = new CostClassVisitor(cw)
+                            cr.accept(cv, ClassReader.EXPAND_FRAMES)
+                            byte[] code = cw.toByteArray()
+                            FileOutputStream fos = new FileOutputStream(
+                                    file.parentFile.absolutePath + File.separator + name);
+                            fos.write(code)
+                            fos.close()
+                        }
+                    }
+                }
+
+                def dest = outputProvider.getContentLocation(directoryInput.name,
+                        directoryInput.contentTypes, directoryInput.scopes,
+                        Format.DIRECTORY)
+                FileUtils.copyDirectory(directoryInput.file, dest)
+            }
+            input.jarInputs.each { JarInput jarInput ->
+                def jarName = jarInput.name
+                def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length() - 4)
+                }
+                def dest = outputProvider.getContentLocation(jarName + md5Name,
+                        jarInput.contentTypes, jarInput.scopes, Format.JAR)
+                FileUtils.copyFile(jarInput.file, dest)
+            }
+        }
     }
 }
