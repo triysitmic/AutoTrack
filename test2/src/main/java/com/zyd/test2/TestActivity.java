27ae0814ce4d306f82b0c2f726fb5fca49271406
zhengyanda <zhengyanda@meituan.com>
2019-01-11 18:05:47 +0800

    支持多module代码织入
    Signed-off-by: zhengyanda <zhengyanda@meituan.com>

--- test2/src/main/java/com/zyd/test2/TestActivity.java
+++ test2/src/main/java/com/zyd/test2/TestActivity.java
@@ -1,22 +1,15 @@
-package com.zyd.autotrack;
+package com.zyd.test2;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.zyd.interceptor.ClickInterceptor;
-import com.zyd.test2.TestActivity;
-
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class TestActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ClickInterceptor.setDefaultInterceptTime(500);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.test_main);
         findViewById(R.id.btn1).setOnClickListener(this);
         findViewById(R.id.btn2).setOnClickListener(this);
         findViewById(R.id.btn3).setOnClickListener(this);
@@ -27,7 +20,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        Intent intent = new Intent(MainActivity.this, TestActivity.class);
-        startActivity(intent);
+
     }
 }
--- track/src/main/groovy/com/zyd/track/AutoTrackPlugin.groovy
+++ track/src/main/groovy/com/zyd/track/AutoTrackPlugin.groovy
@@ -9,8 +9,15 @@ import com.android.tools.r8.org.objectweb.asm.ClassWriter
 import com.android.utils.FileUtils
 import com.zyd.track.asm.ClickClassVisitor
 import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.compress.utils.IOUtils
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+
+import javax.lang.model.element.QualifiedNameable
+import java.util.jar.JarEntry
+import java.util.jar.JarFile
+import java.util.jar.JarOutputStream
+import java.util.zip.ZipEntry
 
 public class AutoTrackPlugin extends Transform implements Plugin<Project> {
 
@@ -51,21 +58,8 @@ public class AutoTrackPlugin extends Transform implements Plugin<Project> {
         inputs.each { TransformInput input ->
             input.directoryInputs.each { DirectoryInput directoryInput ->
                 if (directoryInput.file.isDirectory()) {
-
                     directoryInput.file.eachFileRecurse { File file ->
-                        def name = file.name
-                        if (name.endsWith(".class") && !name.startsWith("R\$") &&
-                                "R.class" != name && "BuildConfig.class" != name) {
-                            ClassReader cr = new ClassReader(file.bytes)
-                            ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS)
-                            ClassVisitor cv = new ClickClassVisitor(cw)
-                            cr.accept(cv, ClassReader.EXPAND_FRAMES)
-                            byte[] code = cw.toByteArray()
-                            FileOutputStream fos = new FileOutputStream(
-                                    file.parentFile.absolutePath + File.separator + name)
-                            fos.write(code)
-                            fos.close()
-                        }
+                        inject(file)
                     }
                 }
 
@@ -82,9 +76,69 @@ public class AutoTrackPlugin extends Transform implements Plugin<Project> {
                 }
                 def dest = outputProvider.getContentLocation(jarName + md5Name,
                         jarInput.contentTypes, jarInput.scopes, Format.JAR)
-                FileUtils.copyFile(jarInput.file, dest)
+
+                File newFile = modifyJar(jarInput.file, context.temporaryDir)
+                FileUtils.copyFile(newFile, dest)
             }
         }
     }
+
+    static File modifyJar(File jarFile, File tempDir) {
+        def file = new JarFile(jarFile)
+        def md5name = DigestUtils.md5Hex(jarFile.absolutePath).substring(0, 8)
+        def outputJar = new File(tempDir, md5name + jarFile.name)
+        JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(outputJar))
+        Enumeration enumeration = file.entries()
+        while (enumeration.hasMoreElements()) {
+            JarEntry jarEntry = (JarEntry) enumeration.nextElement()
+            InputStream inputStream = file.getInputStream(jarEntry)
+
+            String entryName = jarEntry.getName()
+            ZipEntry zipEntry = new ZipEntry(entryName)
+
+            jarOutputStream.putNextEntry(zipEntry)
+
+            byte[] modifiedClassBytes = null
+            byte[] sourceClassBytes = IOUtils.toByteArray(inputStream)
+            if (entryName.endsWith(".class")) {
+                modifiedClassBytes = modify(sourceClassBytes)
+            }
+            if (modifiedClassBytes == null) {
+                jarOutputStream.write(sourceClassBytes)
+            } else {
+                jarOutputStream.write(modifiedClassBytes)
+            }
+            jarOutputStream.closeEntry()
+        }
+        jarOutputStream.close()
+        file.close()
+        return outputJar
+    }
+
+    private static void inject(File file) {
+        def name = file.name
+
+        if (name.endsWith(".class") && !name.startsWith("R\$") &&
+                "R.class" != name && "BuildConfig.class" != name) {
+
+            def code = modify(file.bytes)
+            if (code != null) {
+                FileOutputStream fos = new FileOutputStream(
+                        file.parentFile.absolutePath + File.separator + name)
+                fos.write(code)
+                fos.close()
+            }
+
+        }
+    }
+
+    private static byte[] modify(byte[] bytes) {
+        ClassReader cr = new ClassReader(bytes)
+        ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS)
+        ClassVisitor cv = new ClickClassVisitor(cw)
+        cr.accept(cv, ClassReader.EXPAND_FRAMES)
+        return cw.toByteArray()
+    }
+
 
 }
